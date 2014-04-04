bd0615acecf9ebb71d854f41b8c1de36d3872155
Suresh V <suresh@grafware.com>
2014-04-04 15:59:20 +0530

    ready to release

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-[[Introduction]]
+# Introduction
 
-============
+Coreloadtests contains 3 tests:
+
+1. ContentCreation
+
+2. Readonly
+
+3. Writeheavy
+
+## Usage:
 
 1. Add to buildout
 
@@ -8,7 +16,6 @@
 
 3. Use quickinstaller to install collective.coreloadtests contentcreation
 
-4. Run test
-
+4. Run test: bin/test -s collective.coreloadtests
 
 
--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1'
+version = '0.2dev'
 
 setup(name='collective.coreloadtests',
       version=version,
       description="Plone core load test plans",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.md").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
